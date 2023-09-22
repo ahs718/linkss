ade2641c7b8a7d1d6c1b5b27dcac33dc08587794
Aiden Schulman <aidenhschulman@gmail.com>
2023-09-21 23:43:56 -0400

    created login page

---
+++ components/TextInput.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <input
+            :id="placeholder"
+            :placeholder="placeholder"
+            :maxlength="String(max)"
+            class="w-full bg-[#eff0eb] text-gray-800 border-2 text-sm border-[#eff0eb] rounded-xl py-3.5 px-3 placeholder-gray-500 focus:outline-none"
+            @focus="($event) => (isFocused = true)"
+            @blur="($event) => (isFocused = false)"
+            :class="isFocused ? 'border-gray-900' : ''"
+            :type="inputType"
+            v-model="inputComputed"
+            autocomplete="off"
+        />
+
+        <span v-if="error" class="text-red-500 text-sm font-semibold">
+            {{ error }}
+        </span>
+    </div>
+</template>
+
+<script setup>
+const emit = defineEmits(["update:input"]);
+const props = defineProps([
+    "input",
+    "placeholder",
+    "max",
+    "inputType",
+    "error",
+]);
+const { input, placeholder, max, inputType, error } = toRefs(props);
+
+let isFocused = ref(false);
+
+const inputComputed = computed({
+    get: () => input.value,
+    set: (val) => emit("update:input", val),
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,63 @@
 <template>
     <div>
-        <AuthLayout> </AuthLayout>
+        <AuthLayout>
+            <div class="mt-10">
+                <h1 class="lg:text-5xl text-3xl text-center font-extrabold">
+                    Log in to your Linktree
+                </h1>
+            </div>
+
+            <form class="mt-12" @submit.prevent="($event) => login()">
+                <div>
+                    <TextInput
+                        placeholder="Email: link@gmail.com"
+                        v-model:input="email"
+                        inputType="email"
+                        :error="errors && errors.email ? errors.email[0] : ''"
+                    />
+                </div>
+
+                <div class="mt-4">
+                    <TextInput
+                        placeholder="Password"
+                        v-model:input="password"
+                        inputType="password"
+                        :error="
+                            errors && errors.password ? errors.password[0] : ''
+                        "
+                    />
+                </div>
+
+                <div class="mt-10">
+                    <button
+                        type="submit"
+                        class="rounded-full w-full p-3 font-bold"
+                        :disabled="!email || !password"
+                        :class="
+                            email && password
+                                ? 'bg-[#8228d9] hover:bg-[#6c21b3] text-white'
+                                : 'bg-[#eff0eb] text-[#a7aaa2]'
+                        "
+                    >
+                        Log in
+                    </button>
+                </div>
+            </form>
+
+            <div class="text-sm text-center pt-12">
+                Don't have an account?
+                <NuxtLink to="/register" class="text-[#8228d9] underline">
+                    Sign up
+                </NuxtLink>
+            </div>
+        </AuthLayout>
     </div>
 </template>
 
 <script setup>
 import AuthLayout from "~/layouts/AuthLayout.vue";
+
+let email = ref(null);
+let password = ref(null);
+let errors = ref(null);
 </script>
