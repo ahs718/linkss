0c5e5a5f43951939a5deddcbe84b8798db4c00ac
Aiden Schulman <aidenhschulman@gmail.com>
2023-09-23 13:34:41 -0400

    created add and update link overlays

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@
     <div v-if="bgIsGray" class="bg-[#f3f3f1] fixed w-full h-full z-[-1]" />
     <NuxtPage />
 
+    <!-- <UpdateLinkOverlay v-if="isMobile && updatedLinkId" /> -->
+    <UpdateLinkOverlay />
+    <AddLinkOverlay v-if="isMobile && addLinkOverlay" />
     <PreviewOverlay v-if="!isMobile && isPreviewOverlay" />
 </template>
 
---
+++ components/AddLinkOverlay.vue
@@ -0,0 +1,86 @@
+<template>
+    <div
+        id="AddLinkOverlay"
+        class="fixed z-40 top-0 left-0 w-full h-full bg-white px-5"
+    >
+        <div class="flex items-center justify-start w-full py-2">
+            <button
+                type="button"
+                @click="($event) => close()"
+                class="rounded-full"
+            >
+                <Icon
+                    name="material-symbols:arrow-back-sharp"
+                    size="35"
+                    color="#676b5f"
+                />
+            </button>
+
+            <div class="text-lg font-semibold pl-8">Add New Link</div>
+        </div>
+
+        <div class="text-2xl font-semibold pt-10">Enter URL</div>
+
+        <form
+            @submit.prevent="($event) => submit()"
+            class="flex items-center gap-4 w-full pt-4"
+        >
+            <div class="w-full">
+                <TextInput
+                    class="w-full"
+                    placeholder="Name"
+                    v-model:input="name"
+                    inputType="text"
+                    :error="errors && errors.name ? errors.name[0] : ''"
+                />
+
+                <div class="py-1" />
+
+                <TextInput
+                    class="w-full"
+                    placeholder="URL"
+                    v-model:input="url"
+                    inputType="text"
+                    :error="errors && errors.url ? errors.url[0] : ''"
+                />
+            </div>
+
+            <button
+                type="submit"
+                @click="($event) => addLink()"
+                class="rounded-full p-2 bg-[#eff0ea]"
+            >
+                <Icon
+                    name="material-symbols:arrow-forward-sharp"
+                    size="25"
+                    color="#676b5f"
+                />
+            </button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import { useUserStore } from "~/stores/user";
+
+const userStore = useUserStore();
+const { addLinkOverlay } = storeToRefs(userStore);
+
+// onMounted(() => userStore.hidePageOverflow(true, "AdminPage"));
+
+const close = () => (addLinkOverlay.value = false);
+
+let name = ref("");
+let url = ref("");
+let errors = ref(null);
+
+const addLink = async () => {
+    //
+};
+
+onUnmounted(() => {
+    // userStore.hidePageOverflow(false, "AdminPage");
+    addLinkOverlay.value = false;
+});
+</script>
