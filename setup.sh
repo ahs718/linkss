4cc1e0cc6099bb36113ba26f0588dcb1c0b35e73
Aiden Schulman <aidenhschulman@gmail.com>
2023-09-23 23:23:29 -0400

    added setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/ahs718/linkss
+cd linkss || exit
+
+# Install npm packages in linkss-site directory
+echo "Installing npm packages..."
+cd linkss-site || exit
+npm install
+
+# Install composer packages in linkss-api directory
+echo "Installing composer packages..."
+cd ../linkss-api || exit
+composer update
+
+# Copy .env.example to .env
+cp .env.example .env
+echo "Copied .env.example to .env..."
+
+# Generate application key
+php artisan key:generate
+echo "Generated application key and wrote value to .env file"
+
+# Navigate to project root directory
+cd ../
+
+echo "'cd links-api && php artisan serve' to start the back-end server"
+echo "'cd links-site && npm run dev' to start the front-end server"
